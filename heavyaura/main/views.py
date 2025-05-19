d51225f7c35da6a75de655a5a15e103c05e1a2a0
lilitstepanyan19 <lilitstepanyan19>
2025-05-19 22:39:16 +0400

    added list.html

--- heavyaura/main/views.py
+++ heavyaura/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_list_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 def popular_list(request):
@@ -14,3 +15,23 @@ def product_detail(request, slug):
     return render(request, 
                   'main/product/detail.html',
                   {'product' : product})
+
+def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available = True)
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
+    if category_slug:
+        category = get_list_or_404(Category, slug = category_slug)
+        paginator = Paginator(products.filter(category = category), 10)
+        current_page = paginator.page(int(page))
+
+        # products = products.filter(category = category)
+    return render(request, 'main/product/list.html',
+                  {'category' : category,
+                   'categories' : categories,
+                   'products' : current_page,
+                #    'products' : products,
+                   'slug_url' : category_slug})
